fe2bffb1857cf73bec6ad4005c926d8a8532bc34
Charles Meyer <charles.meyer@tufts.edu>
2014-12-13 18:25:24 -0500

    removed bad image resources

--- src/main.c
+++ src/main.c
@@ -25,19 +25,19 @@ static void main_window_load(Window *window) {
         text_layer_set_background_color(s_week_layer, GColorBlack);
         text_layer_set_text_color(s_week_layer, GColorWhite);
         text_layer_set_font(s_week_layer, fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK));
-        text_layer_set_text_alignment(s_week_layer, GTextAlignmentCenter);
+        text_layer_set_text_alignment(s_week_layer, GTextAlignmentLeft);
         
         s_time_layer = text_layer_create(GRect(0, 56, 144, 56));
         text_layer_set_background_color(s_time_layer, GColorWhite);
         text_layer_set_text_color(s_time_layer, GColorBlack);
         text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-        text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+        text_layer_set_text_alignment(s_time_layer, GTextAlignmentRight);
         
         s_date_layer = text_layer_create(GRect(0, 112, 144, 56));
         text_layer_set_background_color(s_date_layer, GColorBlack);
         text_layer_set_text_color(s_date_layer, GColorWhite);
         text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK));
-        text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);        
+        text_layer_set_text_alignment(s_date_layer, GTextAlignmentLeft);        
 
         // Add them as child layers to the Window's root layer
         layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
